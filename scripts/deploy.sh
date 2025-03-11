a57ee8ef46ba50fddbf0098f89a560f25451d2c3
peterajessup <peterajessup@users.noreply.github.com>
2025-03-12 09:32:22 +1100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,25 +21,4 @@ oc create -f mqsc/mqsc.yaml
 oc apply -f melCrr.yaml
 # ----------------------------------
 
-# --------For the Live site-------------
-set +e
-oc project mq
-
-oc delete QueueManager sydqm 
-oc delete route sydroute 
-oc delete secret mqkey sydkey tls-mel
-oc delete configMap mq1-mqsc
-
-set -e
-oc apply -f sydRoute.yaml
-
-oc create secret tls mqkey --cert=./tls/tls.crt --key=./tls/tls.key
-
-# Key and cert for CRR
-oc create secret tls sydkey --cert=./tls/tls.crt --key=./tls/tls.key
-oc create secret generic tls-mel --from-file ./tls/tls.crt 
-oc create -f mqsc/mqscsyd.yaml
-
-
-oc apply -f sydCrr.yaml
 
